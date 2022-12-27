249ee3d0c6f0d73e5bde559c2649040a507de495
Do Trung Duc <duc010298@gmail.com>
2022-12-27 15:02:07 +0700

    update source code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Shell } from './shell/shell.service';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
+  Shell.childRoutes([
+    { path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
+  ]),
+
+  // Fallback when no prior route is matched
+  { path: '**', redirectTo: '', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
