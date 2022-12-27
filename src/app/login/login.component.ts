ec15c4bc2e923faf5a4a82b9f53c54e0d4f40188
Do Trung Duc <duc010298@gmail.com>
2022-12-27 16:03:37 +0700

    update source code

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Title } from '@angular/platform-browser';
+import { ActivatedRoute, Router } from '@angular/router';
+import { finalize, Subscription } from 'rxjs';
+import { AuthenticationService, LoginContext } from '../core/authentication/authentication.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit, OnDestroy {
+  loginForm: FormGroup;
+  isLoading = false;
+  isLoginFailed = false;
+  subscription: Subscription;
 
+  constructor(
+    private authenticationService: AuthenticationService,
+    private formBuilder: FormBuilder,
+    private titleService: Title,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+    this.loginForm = this.formBuilder.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+      rememberMe: [true]
+    });
+  }
+
+  ngOnInit(): void {
+    this.titleService.setTitle('Login');
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  login() {
+    this.isLoginFailed = false;
+    const loginContext: LoginContext = {
+      username: this.loginForm.get('username')!.value,
+      password: this.loginForm.get('password')!.value,
+      remember: this.loginForm.get('rememberMe')!.value
+    };
+
+    if (!this.isLoading) {
+      this.isLoading = true;
+      this.subscription = this.authenticationService
+        .login(loginContext)
+        .pipe(
+          finalize(() => {
+            this.isLoading = false;
+          })
+        )
+        .subscribe({
+          next: () => {
+            this.route.queryParams.subscribe(params => {
+              this.router.navigate([params['redirect'] || '/'], { replaceUrl: true })
+            });
+          },
+          error: () => {
+            this.isLoginFailed = true;
+          }
+        });
+    }
+  }
 }
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -3,6 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 
 @NgModule({
@@ -11,7 +19,15 @@ import { LoginComponent } from './login.component';
   ],
   imports: [
     CommonModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatCheckboxModule,
+    MatIconModule,
+    MatProgressSpinnerModule,
   ]
 })
 export class LoginModule { }
