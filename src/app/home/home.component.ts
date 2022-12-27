92979528ab88ebfe1dc5d65aa16c65c977ae2816
Do Trung Duc <duc010298@gmail.com>
2022-12-27 18:06:31 +0700

    update source code

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,7 +16,7 @@ export class HomeComponent implements AfterViewInit {
   pageSizeOptions: number[] = [25, 50, 100, 200];
 
   listData = [];
-  displayedColumns: string[] = ['index', 'file_name', 'file_size', 'created'];
+  displayedColumns: string[] = ['index', 'file_name', 'file_size', 'created', 'tool'];
 
   constructor(
     private route: ActivatedRoute,
